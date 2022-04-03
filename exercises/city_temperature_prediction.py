a7713866597da16f0b5fd1e816916715e151f203
bolldiaz <matan.dizitser@mail.huji.ac.il>
2022-04-03 17:15:55 +0300

    ex2 done

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -21,22 +21,75 @@ def load_data(filename: str) -> pd.DataFrame:
     -------
     Design matrix and response vector (Temp)
     """
-    raise NotImplementedError()
+    df = pd.read_csv(filename, parse_dates=["Date"]).dropna().drop_duplicates()
+    df['DayOfYear'] = df['Date'].dt.dayofyear
+
+    df = df[df["Month"].isin(range(1, 13)) &
+            df["Day"].isin(range(1, 32))]
+
+    df = df[df["Year"] > 0]
+    df = df[df["Temp"] > -20]
+
+    return df
 
 
 if __name__ == '__main__':
     np.random.seed(0)
+
     # Question 1 - Load and preprocessing of city temperature dataset
-    raise NotImplementedError()
+    df = load_data("ex2/City_Temperature.csv")
 
     # Question 2 - Exploring data for specific country
-    raise NotImplementedError()
+    df["Year"] = df["Year"].astype(str)
+    israel_df = df[df["Country"] == 'Israel']
+    graph = px.scatter(israel_df, x="DayOfYear", y="Temp", color="Year",
+                       title="Israel average daily temperature",
+                       labels={"DayOfYear": 'Day Of the Year', "Temp": "Temp"})
+    graph.write_image("ex2/Israel_temp.png")
+
+    monthly_std = israel_df.groupby('Month').agg({'Temp': 'std'})
+    graph = px.bar(monthly_std, title="Israel daily temperature std per month.", labels={"Temp": "temp std"})
+    graph.write_image("ex2/monthly_std.png")
+    df["Year"] = df["Year"].astype(int)
 
     # Question 3 - Exploring differences between countries
-    raise NotImplementedError()
+    country_monthly_mean_std = df.groupby(by=["Month", "Country"]).Temp.agg(['mean', 'std']).reset_index()
+    graph = px.line(country_monthly_mean_std, x='Month', y='mean', error_y='std', color='Country',
+                    labels={'y': "mean with error of std"}, title="Average monthly temperature, with error bars of std")
+    graph.write_image("ex2/country_monthly_mean_std.png")
 
     # Question 4 - Fitting model for different values of `k`
-    raise NotImplementedError()
+    trainX, trainY, testX, testY = split_train_test(israel_df['DayOfYear'], israel_df['Temp'])
+    loss_records = []
+    for k in range(1, 11):
+        polynomial_fitting = PolynomialFitting(k)
+        polynomial_fitting.fit(trainX, trainY)
+        loss_val = polynomial_fitting.loss(testX, testY)
+        loss_records.append(loss_val)
+        print(f"Test loss of k={k}: {format(loss_val, '.2f')}")
+
+    loss_records = np.around(loss_records, 2)
+    graph = px.bar(x=range(1, 11), y=loss_records,
+                   title="Temp fitting loss as function of k.", labels={"x": "polynom degree", "y":"loss"})
+    graph.write_image("ex2/loss_of_k.png")
 
     # Question 5 - Evaluating fitted model on different countries
-    raise NotImplementedError()
+    polynomial_fitting = PolynomialFitting(5)
+    polynomial_fitting.fit(israel_df['DayOfYear'], israel_df['Temp'])
+
+    countries = list(set(df["Country"].values))
+    loss_records = []
+    for country in countries:
+        country_df = df[df["Country"] == country]
+        loss_records.append(polynomial_fitting.loss(country_df['DayOfYear'], country_df['Temp']))
+
+    loss_records = np.around(loss_records, 2)
+    graph = px.bar(x=countries, y=loss_records, labels={"x": "country", "y": "loss"},
+                   title="Loss after fitting of each country.")
+    graph.write_image("ex2/loss_of_countries.png")
+
+
+
+
+
+
--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -9,6 +9,7 @@ import plotly.express as px
 import plotly.io as pio
 pio.templates.default = "simple_white"
 
+TEST_PERCENTAGE = 0.75
 
 def load_data(filename: str):
     """
@@ -23,7 +24,36 @@ def load_data(filename: str):
     Design matrix and response vector (prices) - either as a single
     DataFrame or a Tuple[DataFrame, Series]
     """
-    raise NotImplementedError()
+    df = pd.read_csv(filename).dropna().drop_duplicates()
+
+
+    # Removal of outliers:
+    df = df[df["bedrooms"] < 20]
+    df = df[df["sqft_lot"] < 1250000]
+    df = df[df["sqft_lot15"] < 500000]
+
+    # Clean samples don't hold those constraints:
+    for feature in ["price", "sqft_living", "sqft_lot", "sqft_above", "yr_built", "sqft_living15", "sqft_lot15"]:
+        df = df[df[feature] > 0]
+    for feature in ["bathrooms", "floors", "sqft_basement", "yr_renovated"]:
+        df = df[df[feature] >= 0]
+    df = df[df["waterfront"].isin([0, 1]) &
+            df["view"].isin(range(5)) &
+            df["condition"].isin(range(1, 6)) &
+            df["grade"].isin(range(1, 15))]
+
+    # Change those values to be categorical:
+    df["decade_built"] = (df["yr_built"] / 10).astype(int)
+    df["zipcode"] = df["zipcode"].astype(int)
+    df = pd.get_dummies(df, prefix='zipcode_', columns=['zipcode'])
+    df = pd.get_dummies(df, prefix='decade_built', columns=['decade_built'])
+
+    df["recently_renovated"] = np.where(df["yr_renovated"] >= np.percentile(df.yr_renovated.unique(), 70), 1, 0)
+
+    df = df.drop(["id", "lat", "long", "date", "yr_built", "yr_renovated"], axis=1)
+
+    df.insert(0, 'intercept', 1, True)
+    return df.drop("price", axis=1), df.price
 
 
 def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") -> NoReturn:
@@ -43,25 +73,64 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") ->
     output_path: str (default ".")
         Path to folder in which plots are saved
     """
-    raise NotImplementedError()
+    # remove all categorical columns (and intercept) which cause a redundant cov=0:
+    X = X.loc[:, ~(X.columns.str.contains('^zipcode_') | X.columns.str.contains('^decade_built_'))].drop("intercept", axis=1)
+
+    for feature in X:
+        p = np.cov(X[feature], y)[0, 1] / (np.std(X[feature]) * np.std(y))
+
+        graph = px.scatter(pd.DataFrame({'x': X[feature], 'y': y}), x="x", y="y", trendline="ols",
+                         title=f"Correlation Between {feature} Values and Response <br>Pearson Correlation {p}",
+                         labels={"x": f"{feature} Values", "y": "Response Values"})
+        graph.write_image("pearson.correlation.%s.png" % feature)
 
 
 if __name__ == '__main__':
     np.random.seed(0)
+
     # Question 1 - Load and preprocessing of housing prices dataset
-    raise NotImplementedError()
+    X, y = load_data("ex2/house_prices.csv")
 
     # Question 2 - Feature evaluation with respect to response
-    raise NotImplementedError()
+    feature_evaluation(X, y)
 
     # Question 3 - Split samples into training- and testing sets.
-    raise NotImplementedError()
+    trainX, trainY, testX, testY = split_train_test(X, y, TEST_PERCENTAGE)
+    train_df = trainX.merge(trainY, left_index=True, right_index=True)
 
     # Question 4 - Fit model over increasing percentages of the overall training data
-    # For every percentage p in 10%, 11%, ..., 100%, repeat the following 10 times:
-    #   1) Sample p% of the overall training data
-    #   2) Fit linear model (including intercept) over sampled set
-    #   3) Test fitted model over test set
-    #   4) Store average and variance of loss over test set
-    # Then plot average loss as function of training size with error ribbon of size (mean-2*std, mean+2*std)
-    raise NotImplementedError()
+    linear_regression = LinearRegression()
+    loss_mean, loss_std = [], []
+
+    for p in range(10, 101):
+        n = round(len(trainY) * (p / 100))
+
+        loss_samples = []
+        for _ in range(10):
+            sampled = train_df.sample(n)
+            linear_regression.fit(sampled.drop("price", axis=1), sampled["price"])
+            loss_samples.append(linear_regression.loss(testX, testY))
+        loss_samples = np.asarray(loss_samples)
+
+        loss_mean.append(loss_samples.mean())
+        loss_std.append(loss_samples.std())
+
+    loss_mean = np.asarray(loss_mean)
+    loss_std = np.asarray(loss_std)
+    x_range = np.linspace(10, 101)
+
+    fig = go.Figure([go.Scatter(x=x_range,
+                                y=loss_mean,
+                                mode='lines'),
+                     go.Scatter(x=x_range,
+                                y=loss_mean + 2 * loss_std,
+                                mode='lines',marker=dict(color='#444'), showlegend=False),
+                     go.Scatter(x=x_range,
+                                y=loss_mean - 2 * loss_std,
+                                mode='lines', marker=dict(color='#444'), showlegend=False,
+                                fillcolor='rgba(68,68,68,0.3)', fill='tonexty')])
+
+    fig.update_xaxes(ticksuffix="%", title_text="percents of training-set")
+    fig.update_yaxes(title_text="loss over test-set")
+    fig.update_layout(title_text="average loss as function of training size with error ribbon")
+    fig.write_image('loss.png')
